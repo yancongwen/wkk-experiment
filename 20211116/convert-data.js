3ce3795790dab18d9c87f3a34fa2cd45cd570668
yancongwen <ycwen1119@163.com>
2021-11-16 15:07:39 +0800

    add

--- 20211116/convert-data.js
+++ 20211116/convert-data.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const path = require('path')
  
 const converter = csv()
-converter.fromFile('./data/testdata.csv')
+converter.fromFile('./data/testdata2.csv')
     .then(list => {
         let result = list.map(item => {
             return {
-                name: item.name,
-                value: [+item['1dx'], +item['2dx']],
+                name: item['标签'],
+                value: [+item['1d'], +item['2d']],
             }
         })
         exportFile(result)
@@ -16,7 +16,7 @@ converter.fromFile('./data/testdata.csv')
 
 // 写文件
 function exportFile(data) {
-    let file = path.join(__dirname, 'data.json');
+    let file = path.join(__dirname, 'data2.json');
     fs.writeFile(file, JSON.stringify(data), function (err) {
         if (err) {
             console.log(err)
